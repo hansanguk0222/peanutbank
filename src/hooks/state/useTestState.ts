aef3bfebb5364f36f0c7e5834a4ea77d52a7c797
hansanguk0222 <hansanguk0222@gmail.com>
2021-05-14 22:54:50 +0900

    [refactor] () => useSelector(...)의 타입을 넣어줌

--- src/hooks/state/useTestState.ts
+++ src/hooks/state/useTestState.ts
@@ -1,12 +1,13 @@
-import { useSelector } from 'react-redux';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
 import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '@/src/store/slices';
+import { TestState } from '@/src/store/slices/test.slice';
 
 const selectTest = createSelector(
   (state: RootState) => state.test,
   (test) => test
 );
 
-const useTestState = () => useSelector(selectTest);
+const useTestState: ReturnType<TypedUseSelectorHook<TestState>> = () => useSelector(selectTest);
 
 export { useTestState };
