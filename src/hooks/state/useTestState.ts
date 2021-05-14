ca68943133b1241eec7906d0b347245f0dd59c95
hansanguk0222 <hansanguk0222@gmail.com>
2021-05-14 22:58:47 +0900

    [refactor] 해당 hook을 읽어오지 못하는 에러 발생으로 수정

--- src/hooks/state/useTestState.ts
+++ src/hooks/state/useTestState.ts
@@ -1,13 +1,12 @@
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '@/src/store/slices';
-import { TestState } from '@/src/store/slices/test.slice';
 
 const selectTest = createSelector(
   (state: RootState) => state.test,
   (test) => test
 );
 
-const useTestState: ReturnType<TypedUseSelectorHook<TestState>> = () => useSelector(selectTest);
+const useTestState = () => useSelector(selectTest);
 
 export { useTestState };
