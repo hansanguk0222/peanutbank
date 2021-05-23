47d3553d8073ac0b6d55fe37d6d6b01488017190
Hansanguk <hansanguk0222@gmail.com>
2021-05-23 17:46:14 +0900

    [chore] prettier 변경으로 인한 파일 수정

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -2,16 +2,7 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 interface ButtonStyleProps {
-  buttonType:
-    | 'leftSideBarButton'
-    | 'changeMonthButton'
-    | 'profileButton'
-    | 'submitButton'
-    | 'cancleButton'
-    | 'closeModalButton'
-    | 'removeItemButton'
-    | 'defaultButton';
-  isHover?: boolean;
+  buttonType: 'leftSideBarButton' | 'changeMonthButton' | 'profileButton' | 'submitButton' | 'cancleButton' | 'closeModalButton' | 'removeItemButton' | 'defaultButton';
   isSelected?: boolean;
 }
 
@@ -26,8 +17,7 @@ const StyledButton = styled.button<ButtonStyleProps>`
   ${(props) =>
     props.buttonType === 'leftSideBarButton' &&
     css<ButtonStyleProps>`
-      background: ${(props) =>
-        !props.isSelected ? props.theme.color.darkBlue1 : props.theme.color.darkBlue2};
+      background: ${(props) => (!props.isSelected ? props.theme.color.darkBlue1 : props.theme.color.darkBlue2)};
       color: ${(props) => props.theme.color.lightBlue};
       font-size: ${(props) => props.theme.size.font.m};
       padding: ${(props) => props.theme.size.padding.xxxs};
@@ -41,8 +31,7 @@ const StyledButton = styled.button<ButtonStyleProps>`
           content: '';
           top: 0;
           left: 0;
-          border-left: ${(props) => props.theme.size.border.BorderRightleftSideBarItem} solid
-            ${(props) => props.theme.color.jade};
+          border-left: ${(props) => props.theme.size.border.BorderRightleftSideBarItem} solid ${(props) => props.theme.color.jade};
           border: 2px solid;
           border-color: ${(props) => props.theme.color.jade};
           height: 100%;
@@ -57,8 +46,7 @@ const StyledButton = styled.button<ButtonStyleProps>`
             content: '';
             top: 0;
             left: 0;
-            border-left: ${(props) => props.theme.size.border.BorderRightleftSideBarItem} solid
-              ${(props) => props.theme.color.jade};
+            border-left: ${(props) => props.theme.size.border.BorderRightleftSideBarItem} solid ${(props) => props.theme.color.jade};
             border: 2px solid;
             border-color: ${(props) => props.theme.color.jade};
             height: 100%;
@@ -67,20 +55,9 @@ const StyledButton = styled.button<ButtonStyleProps>`
     `}
 `;
 
-export const Button: React.FC<ButtonProps> = ({
-  buttonType = 'defaultButton',
-  label,
-  onClick,
-  isHover,
-  isSelected,
-}) => {
+export const Button: React.FC<ButtonProps> = ({ buttonType = 'defaultButton', label, onClick, isSelected }) => {
   return (
-    <StyledButton
-      isSelected={isSelected}
-      onClick={onClick}
-      buttonType={buttonType}
-      isHover={isHover}
-    >
+    <StyledButton isSelected={isSelected} onClick={onClick} buttonType={buttonType}>
       {label}
     </StyledButton>
   );
