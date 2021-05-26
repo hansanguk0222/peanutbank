329e07bfa02d4199156742c3a91147d4549eb002
Hansanguk <hansanguk0222@gmail.com>
2021-05-26 19:34:31 +0900

    [refactor] 중복된 부분 제거

--- src/styles/global-styled.ts
+++ src/styles/global-styled.ts
@@ -1,26 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
-import reset from 'styled-reset';
 
 export const GlobalStyles = createGlobalStyle`
-    ${reset};
     a{
         text-decoration:none;
         color:inherit;
     }
     *{
-        box-sizing:border-box;
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
     }
     body{
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         font-size: 14px;
+        height: 100vh;
     }
     button {
         display: flex;
         justify-content: center;
         align-items: center;
         border: none;
-    }
-    body {
-        height: 100%;
     }
 `;
