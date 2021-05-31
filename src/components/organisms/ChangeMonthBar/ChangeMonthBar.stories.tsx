986b30478d7a9ab42f456fd33beb956a6ab90131
Hansanguk <hansanguk0222@gmail.com>
2021-05-31 22:33:19 +0900

    [feat] 달력의 달을 바꾸는 헤더를 구현

---
+++ src/components/organisms/ChangeMonthBar/ChangeMonthBar.stories.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Story, Meta } from '@storybook/react';
+import { ChangeMonthBar, ChangeMonthBarProps } from './ChangeMonthBar';
+
+export default {
+  title: 'organisms/ChangeMonthBar',
+  component: ChangeMonthBar,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<ChangeMonthBarProps> = () => {
+  const [yearAndMonth, setYearAndMonth] = useState<{ year: number; month: number }>({ year: 2020, month: 1 });
+  const changeYearAndMonth: ({ upOrDown }: { upOrDown: 'up' | 'down' }) => void = ({ upOrDown }: { upOrDown: 'up' | 'down' }) => {
+    const { year, month } = yearAndMonth;
+    if (upOrDown === 'down') {
+      if (month === 1) {
+        setYearAndMonth({ year: year - 1, month: 12 });
+      } else {
+        setYearAndMonth({ year, month: month - 1 });
+      }
+    } else {
+      if (month === 12) {
+        setYearAndMonth({ year: year + 1, month: 1 });
+      } else {
+        setYearAndMonth({ year, month: month + 1 });
+      }
+    }
+  };
+  return (
+    <ChangeMonthBar
+      buttonType="changeMonthButton"
+      inputType="dateInput"
+      leftArrowOnClick={() => changeYearAndMonth({ upOrDown: 'down' })}
+      rightArrowOnClick={() => changeYearAndMonth({ upOrDown: 'up' })}
+      leftIconSrc="./leftarrow.png"
+      rightIconSrc="./rightarrow.png"
+      text={`${yearAndMonth.year}-${yearAndMonth.month}`}
+      readOnly
+    />
+  );
+};
+
+export const ChangeMonthBarTest = Template.bind({});
