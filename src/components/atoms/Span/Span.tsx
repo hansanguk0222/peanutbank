9f6ba252df8cd8692eb39ed77ffe741ddc0cebe0
Hansanguk <hansanguk0222@gmail.com>
2021-05-31 22:32:24 +0900

    [feat] 달력에서 현재 보이는 달에 날짜가 아닌 것을 회색처리 함

--- src/components/atoms/Span/Span.tsx
+++ src/components/atoms/Span/Span.tsx
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components';
 interface StyleSpanProps {
   spanType: 'calendarDate' | 'default';
   day?: number;
+  thisMonth?: boolean;
 }
 
 export interface SpanProps extends StyleSpanProps {
@@ -16,26 +17,35 @@ const StyledSpan = styled.span<StyleSpanProps>`
       font-size: ${(props) => props.theme.size.font.m};
       ${(props) =>
         props.day === 0 &&
+        props.thisMonth &&
         css`
           color: ${(props) => props.theme.color.red1};
         `};
       ${(props) =>
         props.day === 6 &&
+        props.thisMonth &&
         css`
           color: ${(props) => props.theme.color.blue1};
         `};
       ${(props) =>
         props.day !== 0 &&
+        props.thisMonth &&
         props.day !== 6 &&
         css`
           color: ${(props) => props.theme.color.black5};
         `};
+      ${(props) =>
+        !props.thisMonth &&
+        css`
+          color: ${(props) => props.theme.color.black9};
+        `};
     `}
 `;
 
-export const Span: React.FC<SpanProps> = ({ spanType, children, day, onClick }) => {
+export const Span: React.FC<SpanProps> = ({ spanType, children, day, onClick, thisMonth }) => {
+  console.log(thisMonth);
   return (
-    <StyledSpan spanType={spanType} day={day} onClick={onClick}>
+    <StyledSpan spanType={spanType} day={day} onClick={onClick} thisMonth={thisMonth}>
       {children}
     </StyledSpan>
   );
