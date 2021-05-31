3252c2907ba0ac8fe2851cd0e1d1379302ea806b
Hansanguk <hansanguk0222@gmail.com>
2021-05-31 16:14:43 +0900

    [chore] 임시 커밋

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,13 +2,14 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-links'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-links', 'storybook-svgr-react-component'],
   webpackFinal: async (config, { configType }) => {
     config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, '../'),
     };
+
     return config;
   },
 };
