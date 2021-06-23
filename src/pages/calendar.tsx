29c1bf635c0483b7f7956f73193d78409ad89c83
Hansanguk <hansanguk0222@gmail.com>
2021-06-23 22:11:11 +0900

    [chore] 필요하지 않은 파일 삭제

--- src/pages/calendar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Calendar as UserCalendar } from '@/src/components/organisms/Calendar';
-import { makeDatesWithDays } from '@/src/utils';
-import CalendarBox from '@/src/components/container/CalendarBox';
-const Calendar: React.FC = () => <CalendarBox />;
-
-export default Calendar;
