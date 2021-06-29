136a666d2053a64bb85685c1476935cddb2f817f
Hansanguk <hansanguk0222@gmail.com>
2021-06-29 09:11:13 +0900

    [feat] 라벨이랑 input 태그 같이 달린 컴포넌트 생성

---
+++ src/components/molecules/LabelInput/LabelInput.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { ILabel, Label } from '@/src/components/atoms/Label';
+import { Input, InputProps } from '@/src/components/atoms/Input';
+
+export interface ILabelInput extends InputProps, ILabel {
+  labelValue: string;
+}
+
+export const LabelInput: React.FC<ILabelInput> = ({ inputType, labelValue, onChange, readOnly, text, labelType, label, list, incomeOrExpenditure }) => {
+  return (
+    <Label labelType={labelType}>
+      {labelValue}
+      <Input inputType={inputType} onChange={onChange} readOnly={readOnly} text={text} label={label} list={list} incomeOrExpenditure={incomeOrExpenditure} />
+    </Label>
+  );
+};
