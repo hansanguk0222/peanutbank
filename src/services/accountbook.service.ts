73b0bbc6d4561330c3f113251026993d94a2880b
Hansanguk <hansanguk0222@gmail.com>
2021-07-30 11:20:53 +0900

    [refactor] 유저 아이디가 아닌 닉네임으로 해당 부분을 요청하도록 변경

--- src/services/accountbook.service.ts
+++ src/services/accountbook.service.ts
@@ -1,36 +1,64 @@
 import axios from 'axios';
+import API from './API';
+
 interface IAccountService {
   getAccountBook: (any) => any;
-  createLedger: (any) => any;
-}
-
-export const accountBookService: IAccountService = {
-  getAccountBook({ nickname, year, month }: { nickname: string; year: number; month: number }) {
-    if (process.env.NODE_ENV === 'development') {
-      return axios.get(`${process.env.DEV_SERVER_URL}/accountbooks?user=${userId}&year=${year}&month=${month}`);
-    } else {
-      return axios.get(`${process.env.PRO_SERVER_URL}/accountbooks?user=${userId}&year=${year}&month=${month}`);
-    }
-  },
-  createLedger({
-    userId,
+  createLedger: ({
+    id,
+    nickname,
     amount,
     category,
-    date,
+    yyyy,
+    mm,
+    dd,
     description,
     incomeOrExpenditure,
   }: {
-    userId: string;
-    date: string;
+    id?: string;
+    nickname: string;
+    yyyy: string;
+    mm: string;
+    dd: string;
+    incomeOrExpenditure: string;
+    amount: number;
+    category: string;
+    description: string;
+  }) => any;
+}
+
+export const accountBookService: IAccountService = {
+  getAccountBook({ nickname, yyyy, mm }: { nickname: string; yyyy: number; mm: number }) {
+    if (process.env.NODE_ENV === 'development') {
+      return API.get(`${process.env.NEXT_PUBLIC_DEV_SERVER_URL}/accountbooks/users/${nickname}/years/${yyyy}/months/${mm}`);
+    } else {
+      return API.get(`${process.env.NEXT_PUBLIC_PRO_SERVER_URL}/accountbooks/users/${nickname}/years/${yyyy}/months/${mm}`);
+    }
+  },
+  createLedger({
+    id,
+    nickname,
+    amount,
+    category,
+    yyyy,
+    mm,
+    dd,
+    description,
+    incomeOrExpenditure,
+  }: {
+    id?: string;
+    nickname: string;
+    yyyy: string;
+    mm: string;
+    dd: string;
     incomeOrExpenditure: string;
     amount: number;
     category: string;
     description: string;
   }) {
     if (process.env.NODE_ENV === 'development') {
-      return axios.post(`${process.env.DEV_SERVER_URL}/ledger`, { userId, amount, category, date, description, incomeOrExpenditure });
+      return axios.post(`${process.env.NEXT_PUBLIC_DEV_SERVER_URL}/ledgers`, { id, nickname, amount, category, yyyy, mm, dd, description, incomeOrExpenditure });
     } else if (process.env.NODE_ENV === 'production') {
-      return axios.post(`${process.env.PRO_SERVER_URL}/ledger`, { userId, amount, category, date, description, incomeOrExpenditure });
+      return axios.post(`${process.env.NEXT_PUBLIC_PRO_SERVER_URL}/ledgers`, { id, nickname, amount, category, yyyy, mm, dd, description, incomeOrExpenditure });
     }
   },
 };
